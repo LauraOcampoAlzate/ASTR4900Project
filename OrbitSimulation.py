70982a0b702e4caa0074d68ecf322f57dc15abae
LauraOcampoAlzate <locampo1014@gmail.com>
2020-11-11 11:25:44 -0600

    Uploading code

---
+++ OrbitSimulation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 10:45:08 2020
+
+@author: lauraocampo
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def Gravity(M, m, D):
+    
