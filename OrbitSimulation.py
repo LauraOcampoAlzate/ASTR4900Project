66e790fea5057ee9ca5cf16e3968abfe0f096833
LauraOcampoAlzate <locampo1014@gmail.com>
2020-11-16 14:43:29 -0600

    Stage 2 of the Code

--- OrbitSimulation.py
+++ OrbitSimulation.py
@@ -25,6 +25,7 @@ def EOQ(A, E, THET):
 #Initial Conditions
 theta = np.linspace(0, 2*np.pi, 1000)
 SME = 1 #in AU
+SMJ = 5.2 #in AU
 while True:
     try:
         EccE = float(input("What would you like for Earth's eccentricity?"))
@@ -36,19 +37,35 @@ while True:
         continue
     break
 
+while True:
+    try:
+        EccJ = float(input("What would you like for Jupter's eccentricity?"))
+    except ValueError:
+        print("Please pick a number between 0 and 1")
+        continue
+    if EccE < 0.0 and EccE > 1.0:
+        print("Please pick a number between 0 and 1")
+        continue
+    break
+
 EarthR = EOQ(SME, EccE, theta)
+JupiterR = EOQ(SMJ, EccJ, theta)
 
 t = np.arange(0, 1000, 1)
 for i in range(len(t)):
-    EXCoord = EarthR[i] * np.sin(theta)
-    EYCoord = EarthR[i] * np.cos(theta)
+    EXCoord = EarthR * np.sin(theta)
+    EYCoord = EarthR * np.cos(theta)
+    JXCoord = JupiterR * np.sin(theta)
+    JYCoord = JupiterR * np.cos(theta)
 
     plt.figure()
     plt.plot(0, 0, "*", color="gold")
     plt.plot(EXCoord[i], EYCoord[i], ".", color="forestgreen")
-    plt.xlim(-2, 2)
-    plt.ylim(-2, 2)
-    plt.title("Orbit Simulation")
+    plt.plot(JXCoord[i], JYCoord[i], ".", color="orange")
+    plt.xlim(-6, 6)
+    plt.ylim(-6, 6)
+    plt.title("Earth's Orbit with Eccentricity " + str(EccE) +
+              " Jupiter's Orbit with Eccentricity " + str(EccJ))
     plt.xlabel('x')
     plt.ylabel('y')
     plt.savefig(f"Frame{i:03d}.png")
