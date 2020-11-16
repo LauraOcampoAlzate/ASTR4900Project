740a509ebc33120fceaeeee5e0f6725ef1958de6
LauraOcampoAlzate <locampo1014@gmail.com>
2020-11-16 13:59:14 -0600

    A New Update

--- OrbitSimulation.py
+++ OrbitSimulation.py
@@ -11,33 +11,15 @@ import scipy.constants as sc
 import matplotlib.pyplot as plt
 
 
-def EOQ(A, E):
+def EOQ(A, E, THET):
     """This is the Elliptical Orbit Equation (EOQ) where
     we are finding r, the distance from the focus. The equation
     is dependent on A, the semi-major axis, E, the eccentricity, and
     THET, the angle around the orbit. We are also assuming that the orbit
     is at initial angle of 0 degrees."""
-    theta = np.linspace(0, 2*np.pi, 1000)
 
-    #Saving Values every 10 degrees
-    phi = 0
-    for i in range(len(theta)):
-        if (phi % 10) == 0 or i == theta[0]:
-            R = (A * (1 - E**2)) / (1 + E * np.cos(theta))  
-            phi += 1   
+    R = (A * (1 - E**2)) / (1 + E * np.cos(theta))    
     return R
-
-
-def VEL(D, S):
-    """This is the Vis-Viva Equation (VEL) where we are finding the
-    magnitude of the velocity at any point in the elliptical orbit.
-    Here we use G as the gravitational constant, M is the mass of the Sun
-    in kilograms, D is distance from the focus, and S is the semi-major
-    axis."""
-
-    M = 2 * 10**30 #The mass of the Sun in kg
-    V = np.sqrt(sc.G * M * ((2 / D) - (1 / S)))
-    return V
 
 
 #Initial Conditions
@@ -54,23 +36,20 @@ while True:
         continue
     break
 
-EarthR = EOQ(SME, EccE)
-EarthV = VEL(EarthR, SME)
+EarthR = EOQ(SME, EccE, theta)
 
-EXCoord = EarthR * np.sin(theta)
-EYCoord = EarthR * np.cos(theta)
+t = np.arange(0, 1000, 100)
+for i in range(len(t)):
+    EXCoord = EarthR[i] * np.sin(theta)
+    EYCoord = EarthR[i] * np.cos(theta)
 
-for i in range(len(phi)):
     plt.figure()
-    plt.plot(0, 0, "*")
-    plt.plot(EXCoord, EYCoord)
-
+    plt.plot(0, 0, "*", color="gold")
+    plt.plot(EXCoord, EYCoord, color="forestgreen")
     plt.xlim(-2, 2)
     plt.ylim(-2, 2)
     plt.title("Orbit Simulation")
     plt.xlabel('x')
-    plt.ylabel('y')        
-
-    fr = int(i/20)
-    plt.savefig(f"Frame{fr:03d}.png")
+    plt.ylabel('y')
+    plt.savefig(f"Frame{i:03d}.png")
     plt.close()
